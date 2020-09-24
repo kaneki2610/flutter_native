f901ca0cf6181cf6ad823ace66f1aea626f15765
hungtm <manhhungptit261@gmail.com>
2020-09-24 22:54:55 +0700

    first blood

---
+++ ios/Classes/FlutternativePlugin.h
@@ -0,0 +1,4 @@
+#import <Flutter/Flutter.h>
+
+@interface FlutternativePlugin : NSObject<FlutterPlugin>
+@end
---
+++ ios/Classes/FlutternativePlugin.m
@@ -0,0 +1,20 @@
+#import "FlutternativePlugin.h"
+
+@implementation FlutternativePlugin
++ (void)registerWithRegistrar:(NSObject<FlutterPluginRegistrar>*)registrar {
+  FlutterMethodChannel* channel = [FlutterMethodChannel
+      methodChannelWithName:@"flutternative"
+            binaryMessenger:[registrar messenger]];
+  FlutternativePlugin* instance = [[FlutternativePlugin alloc] init];
+  [registrar addMethodCallDelegate:instance channel:channel];
+}
+
+- (void)handleMethodCall:(FlutterMethodCall*)call result:(FlutterResult)result {
+  if ([@"getPlatformVersion" isEqualToString:call.method]) {
+    result([@"iOS " stringByAppendingString:[[UIDevice currentDevice] systemVersion]]);
+  } else {
+    result(FlutterMethodNotImplemented);
+  }
+}
+
+@end
